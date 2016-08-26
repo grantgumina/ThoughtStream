84f00445d5a1e3ce11ea3f11ccdca5860301d28d
Grant Gumina <grantgumina@outlook.com>
2016-08-25 21:56:27 -0700

    sidebar partially functional

--- public/js/custom.js
+++ public/js/custom.js
@@ -4,6 +4,43 @@ var fixedTop = false;
 $(window).scroll(function(e) {
     oVal = ($(window).scrollTop() / 170);
     $(".blur").css("opacity", oVal);
-    
+
 });
 
+// MY CUSTOM CODE
+$(document).ready(function() {
+
+
+    var newTagHTMLString = '<li class="-gg-tag -gg-new-tag">' +
+    '<h6 class="small">#new-tag</h6>'+
+    '<input type="text" value placeholder="New Tag"' +
+    'class="form-control"/>' +
+    '</li>';
+
+    // User clicks new tag button
+    $('#-gg-new-tag-button').click(function() {
+        // TODO - check for empty new tag li's
+
+        var newTagHTMLObject = $.parseHTML(newTagHTMLString);
+
+        var tagCount = $('.-gg-tag').size();
+
+        console.log('tag count: ' + tagCount);
+        $(newTagHTMLObject).attr('id', '-gg-tag-' + tagCount);
+
+        // Watch for user filling out tag
+        $(newTagHTMLObject).keypress(function(e) {
+            if (e.which == 13) {
+                // Enter is pressed
+                var newTagText = $($(newTagHTMLObject).contents()[1]).val();
+
+                $($(newTagHTMLObject).contents()[0]).html(newTagText);
+                $(newTagHTMLObject).removeClass('-gg-new-tag')
+            }
+        });
+
+        $('#-gg-tags-list').append(newTagHTMLObject);
+
+
+    });
+});
