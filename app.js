43adb1d39f81797a6805b7bdecb008e70bc1c909
Grant Gumina <grantgumina@outlook.com>
2016-10-02 16:55:58 -0700

    starting the rewrite. much better. modal window is not working

--- app.js
+++ app.js
@@ -1,62 +1,125 @@
+// register modal component
+Vue.component('modal', {
+  template: '#modal-template'
+})
 
 new Vue({
 
     // We want to target the div with an id of 'events'
-    el: '#events',
+    el: '#thoughstream',
 
     // Here we can register any values or collections that hold data
     // for the application
     data: {
-        event: { name: '', description: '', date: '' },
-        events: []
+        // Flag for showing new document modal window
+        showModal: false,
+
+        // Tag Model
+        tag: { id: '', name: '' },
+        allTags: [],
+
+        // Document Model
+        doc: { title: '', body: '', type: '', tags: [], date: '' },
+
+        // Populating documents with test/dummy data
+        allDocuments: [
+            {
+                'title': 'JBP - MM1',
+                'body': 'Hello world!',
+                'type': 'note',
+                'tags': [
+                    { 'id': 'rexxxkd0', 'name': 'culture' },
+                    { 'id': 'sfg00sad', 'name': 'history' }
+                ],
+                'date': 'Fri May 04 2015 01:17:07 GMT-0700 (PDT)'
+            },
+            {
+                'id': '199437adx',
+                'title': 'My Heritage',
+                'body': 'FOOBAR NOOBAR!',
+                'type': 'essay',
+                'tags': [
+                    { 'id': 'rexxxkd0', 'name': 'culture' },
+                    { 'id': 'x3bxadf2', 'name': 'music' }
+                ],
+                'date': 'Th July 14 2016 05:17:07 GMT-0700 (PDT)'
+            }
+        ],
     },
 
     // Anything within the ready function will run when the application loads
-    ready: function() {
+    mounted: function() {
         // When the application loads, we want to call the method that initializes
         // some data
-        console.log("work please");
-        this.fetchEvents();
+        console.log("FETCHING TAGS");
+        this.fetchTags();
+        console.log("FETCHING DOCUMENT FOR TAGS");
+        this.fetchDocumentsForTag();
     },
 
     // Methods we want to use in our application are registered here
     methods: {
-        // We dedicate a method to retrieving and setting some data
-        fetchEvents: function() {
-            var events = [{
-                id: 1,
-                name: 'TIFF',
-                description: 'Toronto International Film Festival',
-                date: '2015-09-10'
-            }, {
-                id: 2,
-                name: 'The Martian Premiere',
-                description: 'The Martian comes to theatres.',
-                date: '2015-10-02'
-            }, {
-                id: 3,
-                name: 'SXSW',
-                description: 'Music, film and interactive festival in Austin, TX.',
-                date: '2016-03-11'
-            }];
-            // $set is a convenience method provided by Vue that is similar to pushing
-            // data onto an array
-            this.$set('events', events);
+        // Get all tags belonging to the user
+        fetchTags: function() {
+            var tags = [
+                {
+                    "id": "rexxxkd0",
+                    "name": "culture"
+                },
+                {
+                    "id": "sfg00sad",
+                    "name": "history"
+                },
+                {
+                    "id": "x3bxadf2",
+                    "name": "music"
+                }
+            ];
+
+            this.allTags = tags;
+        },
+
+        // Get all of the user's documents for a specific tag
+        fetchDocumentsForTag: function(tagId) {
+
         },
 
         // Adds an event to the existing events array
-        addEvent: function() {
-            if (this.event.name) {
-                this.events.push(this.event);
-                this.event = { name: '', description: '', date: '' };
-            }
+        // addEvent: function() {
+        //     if (this.event.name) {
+        //         this.events.push(this.event);
+        //         this.event = { name: '', description: '', date: '' };
+        //     }
+        // },
+        //
+        // deleteEvent: function(e) {
+        //     console.log(e);
+        //     if (confirm("Are you sure you want to delete this event?")) {
+        //         this.events.splice(e, 1);
+        //     }
+        // },
+
+        // New Document
+        addDocument: function(e) {
+            console.log("ADD DOCUMENT");
         },
 
-        deleteEvent: function(e) {
-            if (confirm("Are you sure you want to delete this event?")) {
-                // $remove is a Vue convenience method similar to splice
-                this.events.$remove(e);
-            }
-        }
+        // Document Filters
+        showAll: function(e) {
+
+        },
+
+        showNotes: function(e) {
+
+        },
+
+        showEssays: function(e) {
+
+        },
+
+        // Helper functions
+        getDocumentPreviewText: function(documentBody) {
+            return documentBody.substring(0, 12);
+        },
     }
 });
