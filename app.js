31793d546335b32fdc8a90d35495555251a5dc1a
Grant Gumina <grantgumina@outlook.com>
2016-10-15 16:30:24 -0700

    Modals show error messages

--- app.js
+++ app.js
@@ -3,12 +3,92 @@ Vue.component('modal', {
     template: '#modal-template',
 });
 
+Vue.component('tag-modal', {
+    template: '#tag-modal',
+
+    data: function() {
+        return {
+            newTag: { id: '', name: '' },
+            errorMessage: '',
+        }
+    },
+
+    computed: {
+        sanitizedNewTag: function() {
+            return { 'id': this.newTag.id, 'name': this.newTag.name.trim() };
+        }
+    },
+
+    methods: {
+        close: function() {
+            this.$emit('close');
+            this.newTag = { id: '', name: '' };
+            this.showTagModal = false;
+            this.errorMessage = '';
+        },
+
+        addTag: function() {
+            var tag = {};
+
+            // Check if tag is null/blank
+            if (!this.newTag || this.sanitizedNewTag.name == "") {
+                this.errorMessage = 'Blank tags not allowed';
+                this.newTag = { id: '', name: '' };
+                return;
+            }
+
+            // Check to make sure tag is just one word
+            if (this.sanitizedNewTag.name.indexOf(" ") != -1) {
+                this.errorMessage = 'Tags must be one word';
+                this.newTag = { id: '', name: '' };
+                return;
+            }
+
+            // Check if document is already tagged with this tag
+            if (this.doc) {
+                for (var i = 0; i < this.doc.tags.length; i++) {
+                    tag = this.doc.tags[i];
+
+                    if (tag.name.toLowerCase() == this.sanitizedNewTag.name.toLowerCase()) {
+                        this.errorMessage = 'Document is already tagged';
+                        this.newTag = { id: '', name: '' };
+                        return;
+                    }
+                }
+            }
+
+            // Search if tag already exists
+            for (var i = 0; i < main.allTags.length; i++) {
+                var t = main.allTags[i];
+
+                // Tag already exists
+                if (t.name.toLowerCase() == this.sanitizedNewTag.name.toLowerCase()) {
+                    this.errorMessage = 'Tag already exists';
+
+                    // Clear input
+                    this.newTag = { id: '', name: '' };
+                    return;
+                }
+            }
+
+            // If we're here, then this is a brand new tag
+            main.allTags.push(this.sanitizedNewTag);
+
+            // Clear input
+            this.newTag = { id: '', name: '' };
+
+            this.close();
+        }
+    }
+});
+
 Vue.component('note-modal', {
     template: '#note-modal',
 
     data: function () {
         return {
             newTag: { id: '', name: '' },
+            errorMessage: '',
         }
     },
 
@@ -28,6 +108,7 @@ Vue.component('note-modal', {
             console.log(this.doc);
 
             this.$emit('close');
+            this.errorMessage = '';
             this.newTag = { id: '', name: '' };
             this.showNoteModal = false;
 
@@ -40,19 +121,19 @@ Vue.component('note-modal', {
 
             // Check for title
             if (this.doc.title == '' || !this.doc.title) {
-                alert('Notes must have a title');
+                this.errorMessage = 'Notes must have a title';
                 return;
             }
 
             // Check for content
             if (this.doc.body == '' || !this.doc.body) {
-                alert('Notes must have content');
+                this.errorMessage = 'Notes must have content';
                 return;
             }
 
             // Check for tags
             if (!this.doc.tags || this.doc.tags.length == 0) {
-                alert('Notes must be tagged');
+                this.errorMessage = 'Notes must be tagged';
                 return;
             }
 
@@ -74,14 +155,14 @@ Vue.component('note-modal', {
 
             // Check if tag is null/blank
             if (!this.newTag || this.sanitizedNewTag.name == "") {
-                console.log("Blank tags not allowed");
+                this.errorMessage = 'Blank tags not allowed';
                 this.newTag = { id: '', name: '' };
                 return;
             }
 
             // Check to make sure tag is just one word
             if (this.sanitizedNewTag.name.indexOf(" ") != -1) {
-                console.log("Tags must be one word");
+                this.errorMessage = 'Tags must be one word';
                 this.newTag = { id: '', name: '' };
                 return;
             }
@@ -92,7 +173,7 @@ Vue.component('note-modal', {
                     tag = this.doc.tags[i];
 
                     if (tag.name.toLowerCase() == this.sanitizedNewTag.name.toLowerCase()) {
-                        console.log('Document is already tagged');
+                        this.errorMessage = 'Document is already tagged';
                         this.newTag = { id: '', name: '' };
                         return;
                     }
@@ -105,7 +186,7 @@ Vue.component('note-modal', {
 
                 // Tag already exists
                 if (t.name.toLowerCase() == this.sanitizedNewTag.name.toLowerCase()) {
-                    console.log('Tag already exists');
+                    this.errorMessage = 'Tag already exists';
                     this.newTag = t;
 
                     // Add new tag to this document
@@ -139,9 +220,14 @@ var main = new Vue({
     // Here we can register any values or collections that hold data
     // for the application
     data: {
-        // Flag for showing new document modal window
+        // Flag for showing modal windows
         showNoteModal: false,
         showNewEssayModal: false,
+        showTagModal: false,
+
+        // Visible Documents Modifiers
+        showJustNotes: false,
+        showJustEssays: false,
 
         // Tag Model
         tag: { id: '', name: '' },
@@ -178,6 +264,7 @@ var main = new Vue({
         // Selected document
         selectedDoc: {},
 
+        // Selected tag
         selectedTag: {},
 
     },
@@ -185,15 +272,28 @@ var main = new Vue({
     computed: {
         visibleDocuments: function() {
             var visibleDocs = [];
+
+            // Only show documents which have the selected tag
             for (var i = 0; i < this.allDocuments.length; i++) {
                 var doc = this.allDocuments[i];
                 var docTags = doc.tags;
 
-                // Only show documents which have the selected tag
+                // Search through the document's tags
                 for (var j = 0; j < docTags.length; j++) {
                     var dt = docTags[j];
-                    // If document has selected tag show it
+
+                    // If document has selected tag so show it
                     if (dt.id == this.selectedTag.id) {
+                        // Check to see if there's a note filter being applied
+                        if (this.showJustNotes && doc.type != 'note') {
+                            break;
+                        }
+                        // Check to see if there's an essay filter being applied
+                        if (this.showJustEssays && doc.type != 'essay') {
+                            break;
+                        }
+
+                        // Add document to visible documents
                         visibleDocs.push(doc);
                     }
                 }
@@ -258,15 +358,18 @@ var main = new Vue({
 
         // Document Filters
         showAll: function(e) {
-
+            this.showJustNotes = false;
+            this.showJustEssays = false;
         },
 
         showNotes: function(e) {
-
+            this.showJustNotes = true;
+            this.showJustEssays = false;
         },
 
         showEssays: function(e) {
-
+            this.showJustEssays = true;
+            this.showJustNotes = false;
         },
 
         openNewNoteModal: function() {
@@ -286,6 +389,7 @@ var main = new Vue({
 
         openNewTagModal: function() {
             console.log(this.allDocuments);
+            this.showTagModal = true;
         },
 
         isTagActive: function(tag) {
