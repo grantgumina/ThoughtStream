652ab14d36b0ef77fdeb7cb6a9a00deb38782769
Grant J. Gumina <grantgumina@gmail.com>
2017-02-05 13:28:26 -0800

    step 1

--- src/js/popup.js
+++ src/js/popup.js
@@ -4,14 +4,47 @@ window.onload = function () {
         el: '#application',
 
         data: {
+            streams: [{name: "culture", documents: [0, 1, 2]}, {name: "music", documents: [0, 1, 2, 3, 4]}],
+            searchValue: "",
         },
 
         computed: {
+            filteredStreams: function() {
+                var names = this.streams.map(function(s) { return s.name });
+                var filteredResults = fuzzy.filter(this.searchValue, names, null);
+                var filteredNames = filteredResults.map(function(el) { return el.string });
+
+                var fs = this.streams.filter(function(s) {
+                    if (filteredNames.includes(s.name)) {
+                        return s;
+                    }
+
+                    return null;
+                });
+
+                return fs;
+            }
         },
 
         methods: {
 
-            getCurrentTabUrl: function(callback) {
+            createNewStream: function() {
+                var names = this.streams.map(function(s) { return s.name });
+
+                if (!names.includes(this.searchValue)) {
+                    var newStream = {
+                        name: this.searchValue,
+                        documents: []
+                    };
+
+                    this.streams.push(newStream);
+                }
+            },
+
+            addDocumentToStream: function(streamName) {
+                console.log(streamName);
+
+                // Get tab URL
                 var queryInfo = {
                     active: true,
                     currentWindow: true
@@ -20,7 +53,7 @@ window.onload = function () {
                 chrome.tabs.query(queryInfo, function(tabs) {
                     var tab = tabs[0];
                     var url = tab.url;
-                    callback(url);
+                    
                 });
             },
 
